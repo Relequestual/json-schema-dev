b21ed1057dc3a20f0c8e40cfc91f5a7f88315c57
Ben Hutton <bh7@sanger.ac.uk>
2019-04-22 20:36:11 +0100

    Good progress but interactive rebase this commit

--- src/App.vue
+++ src/App.vue
@@ -2,35 +2,64 @@
 
   <div id="app">
     <b-container fluid>
-      <b-row>
+      <b-row class="mb-3 col">
         <h1>JSON Schema Check</h1>
       </b-row>
-<!--     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-      <b-row>
-        <b-col>
-          <!-- <textarea id="schema" v-model="schema" cols="100" rows="30" v-on:change="debug($event)"></textarea> -->
-          <editor v-model="schema" @init="editorInit" lang="JSON" theme="chrome" width="100%" height="80"></editor>
+      <b-row class="mb-3">
+        <b-col md="6">
+          <b-row class="mb-3 col">
+            <h2>Schema</h2>
+            <p>JSON Schema
+              <icon v-if="this.schemaJSONValid === true" :icon="['far', 'check-circle']" size="2x"></icon>
+              <icon v-else-if="this.schemaJSONValid === false" :icon="['far', 'times-circle']" size="2x"></icon>
+              <icon v-else :icon="['far', 'question-circle' ]" size="2x"></icon>
+            </p>
+            <p>Some text</p>
+          </b-row>
+          <b-row class="col">
+            <!-- <textarea id="schema" v-model="schema" cols="100" rows="30" v-on:change="debug($event)"></textarea> -->
+            <!-- <editor v-model="schemaText" @init="editorInit" lang="json" theme="chrome" width="100%" height="500"></editor> -->
+            <codemirror ref="schemaCM" v-model="schemaText" :options="cmOption"></codemirror>
+
+          </b-row>
         </b-col>
+        <b-col md="6">
+          <b-row class="mb-3 col">
+            JSON instance Status:  <icon :icon="['far', 'question-circle']" size="2x"></icon>
+          </b-row>
+          <b-row class="col">
+            <editor v-model="instanceText" @init="editorInit" lang="json" theme="chrome" width="100%" height="500"></editor>
+            <!-- <textarea id="data" v-model="instance" cols="100" rows="30" v-on:change="debug($event)"></textarea> -->
+          </b-row>
+        </b-col>
+      </b-row>
+      <b-row class="mb-3">
         <b-col>
-          <editor v-model="instance" @init="editorInit" lang="JSON" theme="chrome" width="100%" height="80%"></editor>
-          <!-- <textarea id="data" v-model="instance" cols="100" rows="30" v-on:change="debug($event)"></textarea> -->
+          <b-button variant="primary" size="lg" v-on:click="validate">Check valid </b-button>
         </b-col>
       </b-row>
       <b-row>
-        <b-col>
-          <button v-on:click="validate">Check valid</button>
-        </b-col>
+        <b-col></b-col>
       </b-row>
     </b-container>
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
+import _ from 'lodash';
 
-var Ajv = require('ajv');
+import 'codemirror/theme/solarized.css'
+import 'codemirror/addon/lint/lint.css';
+import 'codemirror/mode/javascript/javascript.js'
+
+import 'codemirror/addon/lint/lint.js';
+import 'codemirror/addon/lint/json-lint.js';
+
+const Ajv = require('ajv');
 // require('ajv-async')(Ajv);
+
+const jsonlint = require("jsonlint-mod");
+window.jsonlint = jsonlint;
 
 export default {
   name: 'app',
@@ -47,32 +76,88 @@ export default {
       // require('brace/snippets/javascript') //snippet
     },
     validate: function() {
-      this.debug(this.schema);
-      this.debug(this.instance);
+      this.checkingValidation = true
+      this.debug(this.schemaText);
+      this.debug(this.instanceText);
 
-      const jsonSchema = JSON.parse(this.schema);
-      const jsonInstance = JSON.parse(this.instance);
+      try {
+        this.schema = jsonlint.parse(this.schemaText);
+        this.validSchemaJSON = true;
+      } catch(e) {
+        this.validSchemaJSON = false;
+        this.checkingValidation = false
+        console.log(e);
+        return;
+      }
+      const jsonInstance = jsonlint.parse(this.instanceText);
 
       const ajv = Ajv();
-      const validator = ajv.compile(jsonSchema);
+      const validator = ajv.compile(this.schema);
       const result = validator(jsonInstance);
 
       if (result) {
-        console.log('Data is valid', data);
+        console.log('Data is valid', result);
       } else {
         console.log('Validation errors:', validator.errors);
       }
 
+      this.checkingValidation = false
+
     },
     debug: function(data) {
       console.log(data);
-    }
+    },
+    checkJSONValid: function(jsonText, name) {
+      try {
+        const json = jsonlint.parse(jsonText);
+        this[`${name}JSONValid`] = true;
+        return json;
+      } catch(e) {
+        this[`${name}JSONValid`] = false;
+        console.log(e);
+        return e;
+      }
+    },
   },
   data: function () {
     return {
-      schema: "",
-      instance: "" ,
-    }
+      checkingValidation: false,
+      schema: null,
+      schemaText: "",
+      instanceText: "" ,
+      schemaJSONValid: null,
+      schemaJSONErrors: [],
+      cmOption: {
+        mode: 'application/json',
+        theme: 'solarized light',
+        gutters: ["CodeMirror-lint-markers"],
+        tabSize: 4,
+        foldGutter: true,
+        styleActiveLine: true,
+        lineNumbers: true,
+        line: true,
+        lint: false,
+        viewportMargin: Infinity,
+      },
+    };
+  },
+  watch: {
+    schemaText: _.debounce( function (newVal) {
+      // These three lines disable the linter if the content empty, only enabling linting if content has non whitespace content
+      const schemaCM = this.$refs.schemaCM.codemirror;
+      const lint = newVal.trim();
+      schemaCM.setOption("lint", lint);
+
+
+      this.schemaJSONValid = null;
+      const response = this.checkJSONValid(newVal, 'schema');
+      // if (!this.schemaValidJSON){
+      //   this.schemaJSONErrors.push(e)
+      // }
+    }, 1000),
+  },
+  mounted: function() {
+    this.$refs.schemaCM.codemirror.setSize("100%", "500px");
   },
 }
 </script>
@@ -85,5 +170,12 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.vue-codemirror {
+  border: 1px solid #eee;
+  height: 100%;
+  width: 100%;
+  text-align: left;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,45 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
-import BootstrapVue from 'bootstrap-vue'
-Vue.use(BootstrapVue)
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import BootstrapVue from 'bootstrap-vue';
+Vue.use(BootstrapVue);
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
-Vue.config.productionTip = false
+import VueCodemirror from 'vue-codemirror'
+
+// require styles
+import 'codemirror/lib/codemirror.css'
+
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+import {FontAwesomeIcon, FontAwesomeLayers } from '@fortawesome/vue-fontawesome';
+import {
+  faQuestionCircle,
+  faCheckCircle,
+  faTimesCircle,
+} from '@fortawesome/free-regular-svg-icons';
+
+import {
+  faMinusCircle
+} from '@fortawesome/free-solid-svg-icons';
+
+library.add(
+  faQuestionCircle,
+  faCheckCircle,
+  faMinusCircle,
+  faTimesCircle,
+);
+
+Vue.use(VueCodemirror);
+
+Vue.component('icon', FontAwesomeIcon);
+
+Vue.component('icon-layers', FontAwesomeLayers)
+
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
