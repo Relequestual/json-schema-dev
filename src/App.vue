0cbd6321fd7948835b8f2a1292e7905d66fc1556
Ben Hutton <bh7@sanger.ac.uk>
2019-05-14 17:20:08 +0100

    Add ko-fi link

--- src/App.vue
+++ src/App.vue
@@ -33,6 +33,15 @@
             >
               JSON-Schema.org
             </b-nav-item>
+            <a href="https://ko-fi.com/F1F3TKJK" target="_blank">
+              <img
+                height="36"
+                style="border:0px;height:36px;"
+                src="https://az743702.vo.msecnd.net/cdn/kofi3.png?v=2"
+                border="0"
+                alt="Buy Me a Coffee at ko-fi.com"
+              >
+            </a>
           </b-navbar-nav>
         </b-collapse>
       </b-container>
