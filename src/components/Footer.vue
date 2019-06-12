e6b206e34b87c26266f4056adc7acbe5bce5c115
Ben Hutton <bh7@sanger.ac.uk>
2019-06-12 13:14:05 +0100

    Update footer to have GitHub link to the project repo

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,32 +1,33 @@
 <template>
-  <b-container fluid class="bg-light p-3">
-    <b-row>
-      <b-col>
-        <p>
-          Made and maintained by:
-          <a
-            href="https://github.com/Relequestual"
-            target="_blank"
-            class="mr-2"
-          ><icon :icon="['fab', 'github']" class="mr-1" />Relequestual</a>
-          <a
-            href="https://twitter.com/relequestual"
-            target="_blank"
-            class="mr-2"
-          ><icon :icon="['fab', 'twitter']" class="mr-1" />@relequestual</a>
-          Thanks to Sponsors: <a href="https://ko-fi.com/F1F3TKJK" target="_blank">
-            <img
-              height="36"
-              style="border:0px;height:36px;"
-              src="https://az743702.vo.msecnd.net/cdn/kofi3.png?v=2"
-              border="0"
-              alt="Buy Me a Coffee at ko-fi.com"
-            >
-          </a>
-        </p>
-      </b-col>
-    </b-row>
-  </b-container>
+  <div style="height:100px">
+    <b-container fluid class="bg-light p-3 fixed-bottom">
+      <b-row>
+        <b-col>
+          <p>
+            <a
+              href="https://github.com/Relequestual/json-schema-dev"
+              target="_blank"
+              class="mr-2 text-secondary"
+            ><icon :icon="['fab', 'github']" class="mr-1" />jsonschema.dev</a>
+            <a
+              href="https://twitter.com/relequestual"
+              target="_blank"
+              class="mr-2 text-secondary"
+            ><icon :icon="['fab', 'twitter']" class="mr-1" />@relequestual</a>
+            Thanks to Sponsors: <a href="https://ko-fi.com/F1F3TKJK" target="_blank">
+              <img
+                height="36"
+                style="border:0px;height:36px;"
+                src="https://az743702.vo.msecnd.net/cdn/kofi3.png?v=2"
+                border="0"
+                alt="Buy Me a Coffee at ko-fi.com"
+              >
+            </a>
+          </p>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
 
 <script>
