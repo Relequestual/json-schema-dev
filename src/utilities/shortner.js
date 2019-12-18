cc7f1157f3e16a543a2f37514396f29aa4d57b2b
Ben Hutton <bh7@sanger.ac.uk>
2019-12-18 17:24:09 +0000

    Add URL shortner! Shortens URLs Gets the long URL data from an original URL given a short URL

---
+++ src/utilities/shortner.js
@@ -0,0 +1,71 @@
+import LZ from 'lz-string';
+import Yourls from 'yourls';
+
+const yourls = new Yourls('https://l.schema.is', '714173792d')
+
+const compressData = data => LZ.compressToEncodedURIComponent(JSON.stringify(data));
+
+const generateDataURL = data => `/s/${data}`;
+
+const genSaveURL = async ({sharedSchema, sharedInstance}) => {
+
+  const compressedData = compressData({
+    s: sharedSchema,
+    i: sharedInstance,
+  });
+
+  const url =  generateDataURL(compressedData);
+
+  return url;
+};
+
+const shortenURL = async (url) => {
+
+  const shorten = (url) => {
+    return new Promise((resolve, reject) => {
+      yourls.shorten(`https://jsonschema.dev${url}`, function (error, result) {
+        if(error){
+          reject(error);
+        }
+        resolve(result);
+      }
+    )});
+  }
+
+  const result = await shorten(url);
+  return result.url.keyword;
+}
+
+const retrieveDataFromURL = async (urlPath) => {
+  const resolveShort = (urlPath) => {
+    return new Promise((resolve, reject) => {
+      yourls.expand(urlPath, function(error, result) {
+
+        if(error){
+          return reject(error);
+        }
+        if(result.errorCode) {
+          return reject(new Error(`${result.message} - ${result.errorCode}`));
+        }
+
+        //=> "https://github.com/neocotic/yourls-api"
+        const matches = result.longurl.match(/.+\.dev\/s\/(.+)$/);
+        if(matches === null){
+          return reject(new Error('Unable to find short URL'));
+        }else {
+          const data = matches[1];
+          resolve(data);
+        }
+      });
+    });
+  }
+
+  const result = await resolveShort(urlPath);
+  return result;
+};
+
+export default {
+  genSaveURL,
+  shortenURL,
+  retrieveDataFromURL
+};
