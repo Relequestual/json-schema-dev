392b44c93e83d5c1938d6b96e3db0228d3c29873
Ben Hutton <relequestual@gmail.com>
2025-08-01 13:03:35 +0100

    Setup new nuxt 4 app

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/scripts',
+    '@nuxt/test-utils',
+  ],
+});
